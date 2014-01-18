354adcc3c5edcd075cefa6da1080ebd6d2900f8b
Shunji Konishi <shun.konishi@nifty.com>
2014-01-18 13:17:05 +0900

    test

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,23 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import flect.redis.RedisService
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("WS Button"))
+  }
+  
+  val myRedisService = RedisService("redis://@localhost:6379")
+  
+  def echo = WebSocket.using[String] { _ =>
+    val channel = myRedisService.createPubSub("echo")
+    (channel.in, channel.out)
+  }
+  
+  def echoTest = Action { implicit request =>
+    Ok(views.html.echoTest())
   }
 
 }
